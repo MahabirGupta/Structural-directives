b6407e26b4e336cfd11520f3fcd714f367eb6656
MahabirGupta <mahabirg81@gmail.com>
2024-01-18 17:24:06 +0800

    create [ngSwitch] derivative

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   standalone: true,
   imports: [CommonModule],
   template: `  
+
   <h2 *ngIf="displayName; else elseBlock">Codevolution</h2>
   <ng-template #elseBlock><h2>Name is hidden</h2></ng-template>
 
@@ -14,6 +15,12 @@ import { Component, OnInit } from '@angular/core';
                <ng-template #thenBlock><h2>Angular</h2></ng-template>
                <ng-template #elseBlock><h2>Angular is hidden</h2></ng-template>     
 
+               <div [ngSwitch]="color">
+                <div *ngSwitchCase="'red'">You picked red color</div>
+                <div *ngSwitchCase="'blue'">You picked blue color</div>
+                <div *ngSwitchCase="'green'">You picked green color</div>
+                <div *ngSwitchDefault>You did not picked any color</div>
+              </div>
        
                `
                ,
@@ -23,10 +30,12 @@ export class TestComponent implements OnInit {
 
   displayName = false;
 
+  public color = "orange";
+
   constructor(){}
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    // throw new Error('Method not implemented.');
   }
 
 }
